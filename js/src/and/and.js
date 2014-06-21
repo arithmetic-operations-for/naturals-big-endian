26cf24fa1a0071f2d55e09871a0d24c6fedcc8e7
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-06-22 00:46:52 +0200

    add var _and_ tests

--- js/src/and/and.js
+++ js/src/and/and.js
@@ -64,12 +64,11 @@ var land_t = function(r){
 
 	return function (a, a0, a1, b, b0, b1, c, c0, c1) {
 
-		var at = a0 + b1 - b0;
-		var ct = c1 - at + a0;
+		var ct = c0 + b1 - b0;
 
 		while (c0 < ct) c[c0++] = a[a0++] & b[b0++];
 
-		if (b[b1 - 1] < _r) while (c0 < c1) c[c0++] = 0;
+		if (b[b0 - 1] < _r) while (c0 < c1) c[c0++] = 0;
 		else                while (c0 < c1) c[c0++] = a[a0++];
 
 
