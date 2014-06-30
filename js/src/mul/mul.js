ff0a551ea4dd39699d59adcfd684e27ae559a1e7
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-06-30 15:06:14 +0200

    bmul first draft

--- js/src/mul/mul.js
+++ js/src/mul/mul.js
@@ -1,3 +1,6 @@
+
+var bmul_t = function(r){
+
 	/**
 	 * Computes product of two big endian arrays.
 	 * <p>
@@ -5,3 +8,32 @@
 	 * using long multiplication algorithm (the one teached in
 	 * european primary schools)
 	 */
+
+	var mul = function(a, ai, aj, b, bi, bj, c, ci, cj){
+		var ak, ck = --cj, ct, t, u, v;
+
+		while (bj --> bi) {
+			for (ak = aj; ak --> ai;) {
+				t = b[bj] * a[ak];
+				u = t % r;
+				v = c[ck] + u;
+
+				c[ck] = v % r;
+
+				for (ct = ck - 1; v >= r && ct >= ci; --ct) {
+					v = c[ct] + 1;
+					c[ct] = v % r;
+				}
+
+				c[--ck] += (t - u) / r;
+
+				for (ct = ck - 1; v >= r && ct >= ci; --ct) {
+					v = c[ct] + 1;
+					c[ct] = v % r;
+				}
+			}
+			ck = --cj;
+		}
+	};
+
+};
