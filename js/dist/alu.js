211eeb07e1831f3764049fcc2fa55bb44fdb2838
aureooms <aurelien.ooms@gmail.com>
2014-09-02 23:07:12 +0200

    little endian version of mul53 + fixes + coverage for mul53

--- js/dist/alu.js
+++ js/dist/alu.js
@@ -9,7 +9,7 @@
  * COMPUTE THE ABSOLUTE VALUE OF NUMBER n
  */
 
-exports.abs = function abs() {};
+// exports.abs = function abs() {};
 
 
 /* js/src/add */
@@ -672,7 +672,7 @@ var bmul53_t = function(r){
 
 	/**
 	 * Multiply two blocks, result is put in a 1 or 2 blocks big endian array.
-	 * i <= 1, j <= 1, k <= 2
+	 * aj - ai <= 1, bj - bi <= 1, cj - ci <= 2
 	 *
 	 *
 	 * @param {array} a first operand
@@ -688,19 +688,15 @@ var bmul53_t = function(r){
 
 	var mul = function(a, ai, aj, b, bi, bj, c, ci, cj){
 
-		var v, i, j, k;
-
-		i = aj - ai;
-		j = bj - bi;
-		k = cj - ci;
+		var v;
 
 		// EMPTY CASE
-		if (i <= 0 || j <= 0 || k <= 0) return;
+		if (aj <= ai || bj <= bi || cj <= ci) return;
 
 		v = a[ai] * b[bi];
 		c[cj-1] = v % r;
 
-		if (k > 1) {
+		if (cj > ci + 1) {
 			c[cj-2] = (v - c[cj-1]) / r;
 		}
 
@@ -712,6 +708,55 @@ var bmul53_t = function(r){
 
 
 exports.bmul53_t = bmul53_t;
+
+
+/**
+ * /!\ BLOCK MULTIPLICATION RESULT MUST HOLD IN THE JAVASCRIPT NUMBER TYPE (DOUBLE i.e. 53 bits)
+ *
+ * little endian 1 block multiplication
+ *
+ */
+
+var lmul53_t = function(r){
+
+	/**
+	 * Multiply two blocks, result is put in a 1 or 2 blocks little endian array.
+	 * aj - ai <= 1, bj - bi <= 1, cj - ci <= 2
+	 *
+	 *
+	 * @param {array} a first operand
+	 * @param {int} ai a left
+	 * @param {int} aj a right
+	 * @param {array} b second operand
+	 * @param {int} bi b left
+	 * @param {int} bj b right
+	 * @param {array} c result, must be 0 initialized
+	 * @param {int} ci c left
+	 * @param {int} cj c right
+	 */
+
+	var mul = function(a, ai, aj, b, bi, bj, c, ci, cj){
+
+		var v;
+
+		// EMPTY CASE
+		if (aj <= ai || bj <= bi || cj <= ci) return;
+
+		v = a[ai] * b[bi];
+		c[ci] = v % r;
+
+		if (cj > ci + 1) {
+			c[ci+1] = (v - c[ci]) / r;
+		}
+
+	};
+
+	return mul;
+
+};
+
+
+exports.lmul53_t = lmul53_t;
 /* js/src/neg */
 /* js/src/neg/neg.js */
 /**
