4e3ee5db9e3861e8e3a0d7315a32c375bb8ad46d
aureooms <aurelien.ooms@gmail.com>
2014-09-04 22:50:27 +0200

    fixes for div tests + progress on convert

--- js/src/wrap/wrapbin.js
+++ js/src/wrap/wrapbin.js
@@ -29,22 +29,4 @@ var wrapbin = function(fn){
 	};
 };
 
-var wrapmov = function(fn){
-	return function(a, i, j, b, k){
-
-		if(i < 0){
-			k -= i;
-			i = 0;
-		}
-		if(k < 0){
-			i -= k;
-			k = 0;
-		}
-		return fn(a, i, j, b, k);
-
-	};
-};
-
-
 exports.wrapbin = wrapbin;
-exports.wrapmov = wrapmov;
