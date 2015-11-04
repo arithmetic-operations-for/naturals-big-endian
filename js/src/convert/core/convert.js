35fdd78e230a1c6a5f2be183e54ec209e4bd3fda
aureooms <aurelien.ooms@gmail.com>
2015-11-04 17:34:31 +0100

    fix

--- js/src/convert/core/convert.js
+++ js/src/convert/core/convert.js
@@ -2,7 +2,7 @@
 const convert = function ( f , t , a , ai , aj ) {
 
 	const bi = 0 ;
-	const bj = Math.ceil( f / t * ( aj - ai ) ) ;
+	const bj = Math.ceil( Math.log( f ) / Math.log( t ) * ( aj - ai ) ) ;
 	const b = _alloc( bj - bi ) ;
 
 	_convert( f , t , a , ai , aj , b , bi , bj ) ;
