bf3df104dd6308ea5aea0415505931374de041fd
aureooms <aurelien.ooms@gmail.com>
2015-11-08 14:21:46 +0100

    test add

---
+++ test/js/src/1-new/arithmetic/add/_add.js
@@ -0,0 +1,21 @@
+var integer = integerbigendian ;
+
+test( '_add' , function ( assert ) {
+
+	function t ( A , B , C ) {
+
+		var r = 10 ;
+		var a = integer.parse( 10 , r , A ) ;
+		var b = integer.parse( 10 , r , B ) ;
+		var c = integer._zeros( Math.max( a.length , b.length ) + 1 ) ;
+		integer._add( r , a , 0 , a.length , b , 0 , b.length , c , 0 , c.length ) ;
+
+		assert.deepEqual( integer.stringify( r , 10 , c , 0 , c.length ) , C , A + ' + ' + B + ' = ' + C ) ;
+
+	}
+
+	t( '1' , '1' , '2' ) ;
+	t( '47' , '124' , '171' ) ;
+	t( '999' , '1' , '1000' ) ;
+
+} ) ;
