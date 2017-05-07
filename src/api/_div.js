60bf9ffe2223defc832cd3dc29f8ffcd0e984b41
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-07 14:22:39 +0200

    better docs for _div

--- src/api/_div.js
+++ src/api/_div.js
@@ -1,7 +1,27 @@
 import { _div_limb , schoolbook_div , _dc_div } from '../core' ;
 import { THRESHOLD_DIV_DC } from '../core' ;
+
 /**
- * |A| = |C|
+ * Computes the quotient and remainder of two numbers. Uses the most
+ * appropriate algorithms depending on the size of the operands. The remainder
+ * is written to the dividend array. There are a few assumptions made on the
+ * input.
+ *
+ * Input
+ * -----
+ *  - No leading zeros
+ *  - |A| = |C|
+ *
+ * @param {Number} r The base to work with.
+ * @param {Array} a Dividend / Remainder array.
+ * @param {Number} ai a left
+ * @param {Number} aj a right
+ * @param {Array} b Divisor array.
+ * @param {Number} bi b left
+ * @param {Number} bj b right
+ * @param {Array} c Quotient array.
+ * @param {Number} ci c left
+ * @param {Number} cj c right
  */
 export function _div ( r , a , ai , aj , b , bi , bj , c , ci , cj ) {
 
