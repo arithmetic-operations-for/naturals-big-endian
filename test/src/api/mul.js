b60070df0982805720975c6afb3c79059537de0d
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-02 13:11:17 +0200

    :mag: test: Add more failing tests for mul.
    I suspect the misuse of _mul_limb is the only source of doom for these
    new tests.

--- test/src/api/mul.js
+++ test/src/api/mul.js
@@ -31,3 +31,91 @@ test( macro ,
 
 test( macro , '41635564802587630124971701498526931600520671849926893478847011' , '11' , '457991212828463931374688716483796247605727390349195828267317121' ) ;
 test( macro , '11', '41635564802587630124971701498526931600520671849926893478847011' , '457991212828463931374688716483796247605727390349195828267317121' ) ;
+
+
+function boundaries ( t , a , ai , aj , b , bi , bj , C ) {
+
+	const r = 10 ;
+	const c = _zeros( aj - ai + bj - bi ) ;
+
+	mul( r , a , ai , aj , b , bi , bj , c , 0 , c.length ) ;
+
+	const result = stringify( r , 10 , c , 0 , c.length ) ;
+
+	t.is( C , result ) ;
+
+}
+
+boundaries.title = (providedTitle, a , ai , aj , b , bi , bj , C) => providedTitle || `mul(${a.join('')}[${ai}:${aj}],${b.join('')}[${bi}:${bj}]) = ${C}`;
+
+
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 0 , 1 , '1' ) ;
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 0 , 2 , '12' ) ;
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 0 , 3 , '123' ) ;
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 1 , 2 , '2' ) ;
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 1 , 3 , '23' ) ;
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 2 , 3 , '3' ) ;
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 0 , 1 , '12' ) ;
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 0 , 2 , '144' ) ;
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 0 , 3 , '1476' ) ;
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 1 , 2 , '24' ) ;
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 1 , 3 , '276' ) ;
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 2 , 3 , '36' ) ;
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 0 , 1 , '2' ) ;
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 0 , 2 , '24' ) ;
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 0 , 3 , '246' ) ;
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 1 , 2 , '4' ) ;
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 1 , 3 , '46' ) ;
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 2 , 3 , '6' ) ;
+
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 0 , 0 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 1 , 1 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 2 , 2 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 3 , 3 , '0')
+
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 0 , 0 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 1 , 1 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 2 , 2 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 3 , 3 , '0')
+
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 0 , 0 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 1 , 1 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 2 , 2 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 3 , 3 , '0')
+
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 0 , 1 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 0 , 2 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 0 , 3 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 1 , 2 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 1 , 3 , '0')
+test( boundaries , [1,2] , 0 , 0 , [1,2,3] , 2 , 3 , '0')
+
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 0 , 1 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 0 , 2 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 0 , 3 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 1 , 2 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 1 , 3 , '0')
+test( boundaries , [1,2] , 1 , 1 , [1,2,3] , 2 , 3 , '0')
+
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 0 , 1 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 0 , 2 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 0 , 3 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 1 , 2 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 1 , 3 , '0')
+test( boundaries , [1,2] , 2 , 2 , [1,2,3] , 2 , 3 , '0')
+
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 0 , 0 , '0')
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 0 , 0 , '0')
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 0 , 0 , '0')
+
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 1 , 1 , '0')
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 1 , 1 , '0')
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 1 , 1 , '0')
+
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 2 , 2 , '0')
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 2 , 2 , '0')
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 2 , 2 , '0')
+
+test( boundaries , [1,2] , 0 , 1 , [1,2,3] , 3 , 3 , '0')
+test( boundaries , [1,2] , 0 , 2 , [1,2,3] , 3 , 3 , '0')
+test( boundaries , [1,2] , 1 , 2 , [1,2,3] , 3 , 3 , '0')
