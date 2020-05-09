08c0ca0d339fd236836be13784e4bfb5cf5d75e2
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-05-09 17:14:59 +0200

    Move f == t case to convert_keep_zeros.

--- src/core/convert/convert_keep_zeros.js
+++ src/core/convert/convert_keep_zeros.js
@@ -9,11 +9,10 @@ export function convert_keep_zeros ( f , t , a , ai , aj ) {
 	const bj = Math.ceil( Math.log( f ) / Math.log( t ) * ( aj - ai ) ) ;
 	const b = _zeros( bj - bi ) ;
 
-	//if ( f === t ) {
-		//_copy( a , ai , aj , b , bi ) ;
-	//}
-	//else
-		if ( aj - ai >= THRESHOLD_CONVERT_DC ) {
+	if ( f === t ) {
+		_copy( a , ai , aj , b , bi ) ;
+	}
+	else if ( aj - ai >= THRESHOLD_CONVERT_DC ) {
 		_convert_dc( f , t , a , ai , aj , b , bi , bj ) ;
 	}
 	else {
